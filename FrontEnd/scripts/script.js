7240be6bdaa37295905e28fa0b2f54ab35c7043d
Nico Joun <nicojoun793@gmail.com>
2024-03-21 11:20:07 +0100

    feat: ajout d'une fonction en js pour afficher les boutons des filtres

--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -1,26 +1,51 @@
-    // Récupération des projets de l’architecte depuis l'API
-    const reponse = await fetch('http://localhost:5678/api/works');
-    const projets = await reponse.json();
+// Récupération des projets de l’architecte depuis l'API
+const reponse = await fetch('http://localhost:5678/api/works');
+const projets = await reponse.json();
 
-    //affichage des projets en images
+//affichage des projets en images
+function genererProjets(projets) {
     for (let i = 0; i < projets.length; i++) {
         const figure = projets[i]
-
+    
         // Récupération de l'élément du DOM qui accueillera les projets
         const gallery = document.querySelector(".gallery")
-
+    
         // Création d’une balise dédiée à un projet
         const figureProjet = document.createElement("figure")
-
+    
         // création des balises
         const imageProjet = document.createElement("img")
         imageProjet.src = figure.imageUrl
         imageProjet.alt = figure.title
-
+    
         const titreProjet = document.createElement("figcaption")
         titreProjet.innerText = figure.title
-
+    
         gallery.appendChild(figureProjet)
         figureProjet.appendChild(imageProjet)
         figureProjet.appendChild(titreProjet)
     }
+}
+
+//affichage des filtres
+function genererFiltres(projets) {
+    for (let i = 0; i < projets.length; i++) {
+        const filtreProjet = projets[i]
+
+        // Récupération de l'élément du DOM qui accueillera les filtres
+        const filtre = document.querySelector(".filtre")
+
+        // Création d’un bouton dédié à un filtre
+        const boutonFiltre = document.createElement("button")
+        boutonFiltre.innerHTML = filtreProjet.category.name
+
+        filtre.appendChild(boutonFiltre)
+    }
+}
+
+//appel des fonctions
+genererProjets(projets)
+genererFiltres(projets)
+
+
+

b0d456d070d22c22f571d537d1c7b41ac0392859
Nico Joun <nicojoun793@gmail.com>
2024-03-28 10:59:12 +0100

    feat: affichage du message d'erreur en cas de mauvaise combinaison mail - mot de passe

--- FrontEnd/scripts/login.js
+++ FrontEnd/scripts/login.js
@@ -1,21 +1,40 @@
 //connexion
 export function connexion() {
-    const formulaireConnexion = document.querySelector("form")
-    formulaireConnexion.addEventListener("submit", (event) => {
-        event.preventDefault()
-        // Création de l’objet de la connexion
-        const login = {
-            email: event.target.querySelector("[name=email]").value,
-            password: event.target.querySelector("[name=password]").value,
-        }
-        // Création de la charge utile au format JSON
-        const chargeUtile = JSON.stringify(login)
-        // Appel de la fonction fetch avec toutes les informations nécessaires
-        fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: chargeUtile
-        });
-    })
+ 
+        const formulaireConnexion = document.querySelector("form")
+        const spanMessageErreur = document.createElement("span")
+
+        formulaireConnexion.addEventListener("submit", (event) => {
+     
+                event.preventDefault()
+                // Création de l’objet de la connexion
+                const login = {
+                    email: event.target.querySelector("[name=email]").value,
+                    password: event.target.querySelector("[name=password]").value,
+                }
+                // Création de la charge utile au format JSON
+                const chargeUtile = JSON.stringify(login)
+                // Appel de la fonction fetch avec toutes les informations nécessaires
+                fetch("http://localhost:5678/api/users/login", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: chargeUtile
+                })
+                .then((response) => {
+                    console.log(response.status)
+                    if (response.status === 200) {
+                        console.log("bravo !!!!!!!!!")
+                        spanMessageErreur.innerHTML = ""
+                    } else  {
+                        spanMessageErreur.innerHTML = ""
+                        formulaireConnexion.appendChild(spanMessageErreur)
+                        const messageErreur = document.createElement("p")
+                        messageErreur.innerText = "Erreur dans l’identifiant ou le mot de passe"
+                        spanMessageErreur.appendChild(messageErreur)
+                    }  
+                })
+
+         })
+
 }
 connexion()
